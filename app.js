03e3301e0108294a3b00f38aa8e2304353c2ca41
Manmeet <mnmtsnghh@gmail.com>
2025-02-09 13:47:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const cacheRoutes = require("./routes/cacheRoutes");
+const globalErrorHandler = require("./controllers/errorController");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1/cache", cacheRoutes);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
